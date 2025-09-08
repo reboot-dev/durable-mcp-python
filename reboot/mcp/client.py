79d18f3a14177126f2d797503d7e9ad35b2e7859
Benjamin Hindman <benjamin.hindman@gmail.com>
2025-09-08 11:21:26 -1000

    Configure `httpx` to retry so tests are not flaky

--- reboot/mcp/client.py
+++ reboot/mcp/client.py
@@ -3,6 +3,7 @@ Helpers for writing clients.
 """
 
 import asyncio
+import httpx
 import mcp
 import mcp.types
 from contextlib import asynccontextmanager
@@ -12,6 +13,34 @@ from mcp.server.streamable_http import (
     MCP_SESSION_ID_HEADER,
 )
 from typing import AsyncIterator, Any, Callable
+
+
+def create_mcp_http_client(
+    headers: dict[str, str] | None = None,
+    timeout: httpx.Timeout | None = None,
+    auth: httpx.Auth | None = None,
+) -> httpx.AsyncClient:
+    """
+    Almost identical implementation to
+    `mcp.shared._httpx_utils.create_mcp_http_client`, but we've added
+    a transport which does retries.
+    """
+    kwargs: dict[str, Any] = {
+        "follow_redirects": True,
+    }
+
+    kwargs["timeout"] = timeout or httpx.Timeout(30.0)
+
+    if headers is not None:
+        kwargs["headers"] = headers
+
+    if auth is not None:
+        kwargs["auth"] = auth
+
+    return httpx.AsyncClient(
+        transport=httpx.AsyncHTTPTransport(retries=5),
+        **kwargs,
+    )
 
 
 @asynccontextmanager
@@ -25,6 +54,7 @@ async def connect(
         url,
         headers=headers,
         terminate_on_close=terminate_on_close,
+        httpx_client_factory=create_mcp_http_client,
     ) as (read_stream, write_stream, get_session_id):
         async with mcp.ClientSession(read_stream, write_stream) as session:
             yield session, get_session_id
